789a47461fa5b074c0c26ef2db8732c1dbc69e07
FujitaKentaro <k021g1261@g.neec.ac.jp>
2024-07-31 10:30:02 +0900

    一旦フェーズ切り替え入れた

--- UserApplication/UI.h
+++ UserApplication/UI.h
@@ -25,10 +25,14 @@ public:
 	void SetTimeGauge(const int32_t& timeGauge) {
 		timeGauge_ = timeGauge;
 	}
+	void SetNowPhase(const int32_t& nowPhase) {
+		nowPhase_ = nowPhase;
+	}
 	float lerpFloat(const float start,const float end,const float t);
 
 private:
 	void PlayerHPUpdate();	// player関連
+	void TimeUpdate();
 public:
 
 
@@ -42,6 +46,8 @@ private:
 	bool downPLHp_ ;
 
 	std::unique_ptr<Sprite> timeSprite_ = nullptr;
+	Vector4 timeColor_;
 	int32_t timeGauge_;
+	int32_t nowPhase_;
 };
 
