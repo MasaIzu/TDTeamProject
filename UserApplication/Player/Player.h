9bbd1a51799d119099ed251ff045c42b084cd735
masato izumida <Oioi_0126@outlook.com>
2024-08-04 18:50:43 +0900

    dasu

--- UserApplication/Player/Player.h
+++ UserApplication/Player/Player.h
@@ -225,7 +225,7 @@ private:
 	BaseCollider* playerCloseCollider;
 	CollisionManager* collisionManager = nullptr;
 	float playerRadius = 0.5f;
-	float playerColseRadius = 20.0f;
+	float playerColseRadius = 200.0f;
 
 #pragma endregion
 
